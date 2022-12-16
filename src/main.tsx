d2d06947ae9c5ccf63c745cf9b79cf25e9515d85
ElvinYekayev <elvinyeka@gmail.com>
2022-12-17 02:40:41 +0400

    hello

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import store from "./store";
 
-
+//hello
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
